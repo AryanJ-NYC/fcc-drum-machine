15c320b39d5961c8f7619abade3609d0ac948b06
Aryan Jabbari <aryan.jabbari21@qmail.cuny.edu>
2018-08-25 14:41:35 -0400

    Play audio on key press

--- src/containers/DrumKit.js
+++ src/containers/DrumKit.js
@@ -1,14 +1,31 @@
-import React, { Component } from 'react';
+import React, { createRef, Component, Fragment } from 'react';
+import EventListener from 'react-event-listener';
 import { Column, Columns } from 'bloomer';
 import DrumPad from '../components/DrumPad';
 import drumData from '../data/drums.json';
 
 export default class DrumKit extends Component {
-  renderDrumPads() {
+  constructor(props) {
+    super(props);
+
+    // in order to play drum audio on key press,
+    // a reference to each DrumPad is stored in an object
+    this.drumPadRefs = drumData.reduce((prev, drum) => {
+      prev[drum.key] = createRef();
+      return prev;
+    }, {});
+  }
+
+  handleKeyDown = (e) => {
+    const keyPressed = e.key.toUpperCase();
+    this.drumPadRefs[keyPressed].current.onClick();
+  }
+
+  renderDrumPads = () => {
     return drumData.map(drum => (
       <Column isSize='1/3' key={drum.key}>
         <div className="has-text-centered">
-          <DrumPad drum={drum}></DrumPad>
+          <DrumPad drum={drum} ref={this.drumPadRefs[drum.key]} />
         </div>
       </Column>
     ));
@@ -16,9 +33,15 @@ export default class DrumKit extends Component {
 
   render() {
     return (
-      <Columns id="display" isCentered isMultiline>
-        {this.renderDrumPads()}
-      </Columns>
+      <Fragment>
+        <EventListener
+          target={document}
+          onKeyDown={this.handleKeyDown}
+        />
+        <Columns id="display" isCentered isMultiline>
+          {this.renderDrumPads()}
+        </Columns>
+      </Fragment>
     );
   }
 }
